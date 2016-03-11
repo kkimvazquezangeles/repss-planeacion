7ddb8f412ea53a98f61ade401794203187153649
kkimvazquezangeles <kkimvazquezangeles@hotmail.com>
2016-03-11 09:40:09 -0600

    Modificando estilos

---
+++ repositories/src/main/resources/com/codigoartesanal/lupa/scripts/h2/dml/persona.sql
@@ -0,0 +1,9 @@
+INSERT INTO DEPARTAMENTO (ID, NOMBRE) VALUES
+(1, 'Direccion General'),
+(2, 'Direccion dos'),
+(3, 'Direccion tres'),
+(4, 'Direccion cuatro'),
+(5, 'Direccion cinco'),
+(6, 'Direccion seis'),
+(7, 'Direccion siete'),
+(8, 'Direccion ocho');
